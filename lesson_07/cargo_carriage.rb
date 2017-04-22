1d5cbe740dd039f5a26e7a98abf48c6b6cd8ff96
Sergey Vishniakov <sergey.vishniakov@gmail.com>
2017-04-22 16:26:52 +0200

    Cargo cargiage adjustements according to task

--- lesson_07/cargo_carriage.rb
+++ lesson_07/cargo_carriage.rb
@@ -1,2 +1,16 @@
 class CargoCarriage < Carriage
+  attr_reader :free_volume
+
+  def initialize(volume)
+    @volume = volume.to_f
+    @free_volume = @volume
+  end
+
+  def take_volume(volume)
+    @free_volume -= volume unless (@free_volume - volume) <= 0
+  end
+
+  def taken_volume
+    @volume - @free_volume
+  end
 end
